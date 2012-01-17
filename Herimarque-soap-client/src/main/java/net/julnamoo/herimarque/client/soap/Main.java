8ed0ca62415fdf5be3bc391c30cb15d9be69eddf
julie kim <kjule114@gmail.com>
2012-01-18 04:19:38 +0900

    W3C document로 request xml build엔 성공했으나 01(APPLICATION ERROR)코드가 리턴됨

--- Herimarque-soap-client/src/main/java/net/julnamoo/herimarque/client/soap/Main.java
+++ Herimarque-soap-client/src/main/java/net/julnamoo/herimarque/client/soap/Main.java
@@ -3,18 +3,19 @@ package net.julnamoo.herimarque.client.soap;
 import iros.gsb.constant.WebSvcType;
 import iros.gsb.sbe.api.IntegrationClientAPI;
 
-import java.io.File;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.util.List;
-import java.util.Map;
+import java.io.StringWriter;
 
-import org.apache.commons.io.FileUtils;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.input.SAXBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * Enterprise Application Client main class.
@@ -23,43 +24,96 @@ import org.slf4j.LoggerFactory;
 public class Main {
 
 	static Logger logger = LoggerFactory.getLogger(Main.class.getSimpleName());
-	
+
 	public static void main( String[] args ) throws Exception {
-		/*System.out.println( "Hello World Enterprise Application Client!" );
-
-		SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
-		SOAPConnection soapConnection = soapConnectionFactory.createConnection();
-
-		MessageFactory messageFactory = MessageFactory.newInstance(SOAPConstants.SOAP_1_2_PROTOCOL);
-		SOAPMessage soapMessage = messageFactory.createMessage();
-
-		SOAPPart soapPart = soapMessage.getSOAPPart();
-		SOAPEnvelope soapEnvelope = soapPart.getEnvelope();
-
-		SOAPHeader header = soapEnvelope.getHeader();
-		SOAPElement headElement = header.addChildElement(soapEnvelope.createName("ComMsgHeader", "head", "http://apache.org/headers"));
-		headElement.addChildElement("ServiceKey").addTextNode("J0dBIBcUMESTQ7Rs7bt4U+AMcATmUzND5pvbuT3u7LLSY1Qy0AwcZFgtFNuEtkFoE7Xb6WlA1pionwBEYCsJiQ==");
-
-		SOAPBody body = soapEnvelope.getBody();
-		SOAPElement bodyElement = body.addChildElement(soapEnvelope.createName("getAgeCrltsList", "open", "http://openapi.cha.go.kr/openapi/soap/crlts/AgeCrltsService"));
-		SOAPElement beLstReq = bodyElement.addChildElement("AgeCrltsList");
-		 */
-		IntegrationClientAPI api = new IntegrationClientAPI();
-		String filename = "";
-
+		/** Request information field**/
+		String id;
+		String serviceKey;
+		String reqURI;
 		
-//		filename = "age_request.xml";
-		filename="spec_request.xml";
+		/** For client setup **/
+		IntegrationClientAPI apiClient;
+		String service;
 		
-		WebSvcType wstype = WebSvcType.SOAP;
-//		String url = "http://openapi.cha.go.kr:80/openapi/soap/crlts/AgeCrltsService";
-		String url = "http://openapi.cha.go.kr:80/openapi/soap/crlts/AreaCrltsService";
-		String reqStr = FileUtils.readFileToString(new File(filename));
-		Map headerCnt = null;
+		apiClient = new IntegrationClientAPI();
+		service = "area";
+		id = "netinamu42";
+		serviceKey = "6i1zuQWddVTMFSqlHyhnbw8VL7uvfuHHt3Pth12H3VuFrUvdzFRcfYBZRfTE0Eidf/hV3otAz36MR1F/VDWUDw==";
+		
+		reqURI = "http://openapi.cha.go.kr:80/openapi/soap/crlts/AreaCrltsService";
 
-		String rep = api.send(wstype, url, reqStr, headerCnt);
+		Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+
+		// set up root element
+		Element rootElement = document.createElement("soapenv:Envelope");
+		rootElement.setAttribute("xmlns:soapenv", "http://schemas.xmlsoap.org/soap/envelope/");
+		rootElement.setAttribute("xmlns:head", "http://apache.org/rootElements");
+		rootElement.setAttribute("xmlns:ser", "http://service.areacrlts.crlts.cha/");
+		document.appendChild(rootElement);
+
+
+		//set up header parameter
+		rootElement.appendChild(setupHeader(document));
+
+		//set up body parameter
+		rootElement.appendChild(setupBody(document));
+
+		TransformerFactory transfac = TransformerFactory.newInstance();
+		Transformer transformer = transfac.newTransformer();
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+		//		StreamResult result = new StreamResult(new FileWriter(new File("request.xml")));
+		//		StreamResult result = new StreamResult(new BufferedWriter(new OutputStreamWriter(System.out)));
+		StringWriter writer = new StringWriter();
+		StreamResult result = new StreamResult(writer);
+		DOMSource source = new DOMSource(document);
+		transformer.transform(source, new StreamResult(writer));
+
+
+		String msg = writer.toString();
+
+		String rcv = apiClient.send(WebSvcType.SOAP, reqURI, msg, null);
+		System.out.println(rcv);
+	}
+	
+	private static Element setupHeader(Document document)
+	{
+		Element headerEnv = document.createElement("soapenv:Header");
+		Element header = document.createElement("head:ComMsgHeader");
 		
-		FileUtils.writeStringToFile(new File("response.xml"), rep, "UTF-8");
+		Element requestMsgID = document.createElement("RequestMsgID");
+		requestMsgID.setTextContent("netinamu42");
+		header.appendChild(requestMsgID);
 		
+		Element serviceKey = document.createElement("ServiceKey");
+		serviceKey.setTextContent("6i1zuQWddVTMFSqlHyhnbw8VL7uvfuHHt3Pth12H3VuFrUvdzFRcfYBZRfTE0Eidf/hV3otAz36MR1F/VDWUDw==");
+		header.appendChild(serviceKey);
+		
+		headerEnv.appendChild(header);
+		
+		return headerEnv;
+	}
+	
+	private static Element setupBody(Document document)
+	{
+		Element bodyRoot = document.createElement("soapenv:Body");
+		
+		//set up the service operation for getting list
+		Element operation = document.createElement("ser:getAreaCrltsList");
+		
+		Element arg = document.createElement("arg0");
+		Element ctrdCd = document.createElement("ctrdCd");
+		
+		// need to read excel and set the code number
+		ctrdCd.setTextContent("11");
+		arg.appendChild(ctrdCd);
+		
+		Element pageMg = document.createElement("pageMg");
+		pageMg.setTextContent("100");
+		arg.appendChild(pageMg);
+		
+		operation.appendChild(arg);
+		bodyRoot.appendChild(operation);
+		
+		return bodyRoot;
 	}
 }
--- Herimarque-soap-client/src/test/java/net/julnamoo/herimarque/client/soap/request/TestCreateRequestW3C.java
+++ Herimarque-soap-client/src/test/java/net/julnamoo/herimarque/client/soap/request/TestCreateRequestW3C.java
@@ -1,5 +1,132 @@
 package net.julnamoo.herimarque.client.soap.request;
 
+import java.awt.font.TransformAttribute;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+
+import iros.gsb.constant.WebSvcType;
+import iros.gsb.sbe.api.IntegrationClientAPI;
+
+import javax.naming.spi.DirStateFactory.Result;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.stream.StreamResult;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public class TestCreateRequestW3C {
+	/** Request information field**/
+	public String id;
+	public String serviceKey;
+	public String reqURI;
+	
+	/** For client setup **/
+	private IntegrationClientAPI apiClient;
+	private String service;
+	
+	@Before
+	public void setup() throws Exception
+	{
+		apiClient = new IntegrationClientAPI();
+		service = "area";
+		id = "netinamu42";
+		serviceKey = "6i1zuQWddVTMFSqlHyhnbw8VL7uvfuHHt3Pth12H3VuFrUvdzFRcfYBZRfTE0Eidf/hV3otAz36MR1F/VDWUDw==";
+	}
+	
+	@Test
+	public void areaReqTest() throws Exception
+	{
+		reqURI = "http://openapi.cha.go.kr:80/openapi/soap/crlts/AreaCrltsService";
+		
+		Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+		
+		// set up root element
+		Element rootElement = document.createElement("soapenv:Envelope");
+		rootElement.setAttribute("xmlns:soapenv", "http://schemas.xmlsoap.org/soap/envelope/");
+		rootElement.setAttribute("xmlns:head", "http://apache.org/rootElements");
+		rootElement.setAttribute("xmlns:ser", "http://service.areacrlts.crlts.cha/");
+		document.appendChild(rootElement);
+		
+		
+		//set up header parameter
+		rootElement.appendChild(setupHeader(document));
+		
+		//set up body parameter
+		rootElement.appendChild(setupBody(document));
+		
+		TransformerFactory transfac = TransformerFactory.newInstance();
+		Transformer transformer = transfac.newTransformer();
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+//		StreamResult result = new StreamResult(new FileWriter(new File("request.xml")));
+//		StreamResult result = new StreamResult(new BufferedWriter(new OutputStreamWriter(System.out)));
+		StringWriter writer = new StringWriter();
+		StreamResult result = new StreamResult(writer);
+		DOMSource source = new DOMSource(document);
+		transformer.transform(source, result);
+		
+		String msg = writer.toString();
+		
+		String rcv = apiClient.send(WebSvcType.SOAP, reqURI, msg, null);
+		System.out.println(rcv);
+	}
+	
+	private Element setupHeader(Document document)
+	{
+		Element headerEnv = document.createElement("soapenv:Header");
+		Element header = document.createElement("head:ComMsgHeader");
+		
+		Element requestMsgID = document.createElement("RequestMsgID");
+		requestMsgID.setTextContent(this.id);
+		header.appendChild(requestMsgID);
+		
+		Element serviceKey = document.createElement("ServiceKey");
+		serviceKey.setTextContent(this.serviceKey);
+		header.appendChild(serviceKey);
+		
+		headerEnv.appendChild(header);
+		
+		return headerEnv;
+	}
+	
+	private Element setupBody(Document document)
+	{
+		Element bodyRoot = document.createElement("soapenv:Body");
+		
+		//set up the service operation for getting list
+		Element operation = document.createElement("ser:getAreaCrltsList");
+		
+		Element arg = document.createElement("arg0");
+		Element ctrdCd = document.createElement("ctrdCd");
+		
+		// need to read excel and set the code number
+		ctrdCd.setTextContent("11");
+		arg.appendChild(ctrdCd);
+		
+		Element pageMg = document.createElement("pageMg");
+		pageMg.setTextContent("100");
+		arg.appendChild(pageMg);
+		
+		operation.appendChild(arg);
+		bodyRoot.appendChild(operation);
+		
+		return bodyRoot;
+	}
+	
+	@After
+	public void teardown()
+	{
+	}
 
 }
