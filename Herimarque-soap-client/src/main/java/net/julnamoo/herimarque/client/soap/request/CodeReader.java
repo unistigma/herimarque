0e7bd28a2b0afbc2d2714d710e2c0345521e3419
julie kim <kjule114@gmail.com>
2012-01-18 16:36:13 +0900

    create the test class for extract the code numbers from the excel file. Building Request Class

--- Herimarque-soap-client/src/main/java/net/julnamoo/herimarque/client/soap/request/CodeReader.java
+++ Herimarque-soap-client/src/main/java/net/julnamoo/herimarque/client/soap/request/CodeReader.java
@@ -1,8 +1,18 @@
 package net.julnamoo.herimarque.client.soap.request;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import jxl.Cell;
+import jxl.CellType;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Read the excel file and then return the integer list for code numbers
@@ -12,19 +22,26 @@ import java.util.List;
  */
 public class CodeReader {
 
+	Logger logger = LoggerFactory.getLogger(CodeReader.class.getSimpleName());
+	
 	String service;
 	private File dataFile;
-	List codeList;
+	List<String> codeList;
 	
 	public CodeReader(String service)
 	{
 		this.service = service;
-		loadFile();
-		codeList = new ArrayList<Integer>();
+		codeList = new ArrayList<String>();
 	}
 	
-	private void loadFile()
+	/**
+	 * Load and read the excel file and add numbers to the list as String.
+	 *  
+	 */
+	public void getCodeNumberList()
 	{
+		// It contains only two columns. For getting code number, read only the first column
+		
 		if(this.service.equalsIgnoreCase(ServiceType.AGE.toString()))
 		{
 			this.dataFile = new File("age.xls");
@@ -33,5 +50,49 @@ public class CodeReader {
 		{
 			this.dataFile = new File("area.xls");
 		}
+
+		Workbook workbook = null;
+		Sheet sheet = null;
+		
+		try 
+		{
+			workbook = Workbook.getWorkbook(dataFile);
+		} catch (BiffException e) 
+		{
+			logger.error(e.getMessage(), e.getCause());
+			
+			sheet = null;
+			workbook = null;
+			dataFile = null;
+			return;
+			
+		} catch (IOException e) {
+			logger.error(e.getMessage(), e.getCause());
+			
+			sheet = null;
+			workbook = null;
+			dataFile = null;
+			return;
+		}
+		
+		// get the sheet and add code numbers in the list
+		sheet = workbook.getSheet(0);
+		for(int i = 0; i < sheet.getRows(); ++i)
+		{
+			Cell cell = sheet.getCell(0, i);
+			CellType type = cell.getType();
+			
+			//check the type of the cell
+			if(type == CellType.NUMBER || type == CellType.LABEL)
+			{
+				codeList.add(cell.getContents());
+			}
+		}
 	}
+
+	public List<String> getCodeList() {
+		return codeList;
+	}
+	
+	
 }
