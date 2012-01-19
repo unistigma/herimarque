9066e173e6a51c5d0697ba63b3ba5e75412484ba
julie kim <kjule114@gmail.com>
2012-01-19 15:27:25 +0900

    convert each heritage item to item POJO. It needs to convert to JSON to be written and lucene object to be indexed

--- Herimarque-soap-client/src/main/java/net/julnamoo/herimarque/client/soap/Item.java
+++ Herimarque-soap-client/src/main/java/net/julnamoo/herimarque/client/soap/Item.java
@@ -8,112 +8,225 @@ package net.julnamoo.herimarque.client.soap;
  */
 public class Item {
 
-	String description; //crltsDC
-	String name; //crltsNm
-	String nameChcrt; //crltsNmChcrt
-	String num; //crltsNo
-	String numName; // crltsNoNm
-	String localeCode; // ctrdCd
-	String localeName; // ctrdNm
-	String imgUrl; // imageUrl
-	String imgExist; // imageYn - 1 is exist
-	String itemCode; //itemCd
-	String imgPath; // listImageUrl
-	String cityCode; //signguCd
-	String city; //signguNm
-	String x; //xCnts
-	String y; //yCnts
+	String crltsDc;
+	String crltsNm;
+	String crltsNmChcrt;
+	String crltsNo;
+	String crltsNoNm;
+	String ctrdCd;
+	String ctrdNm;
+	String imageUrl;
+	String imageYn;
+	String listImageUrl;
+	String itemCd;
+	String itemNm;
+	String signguCd;
+	String signguNm;
+	String XCnts;
+	String YCnts;
 	
-	public String getDescription() {
-		return description;
+	public String getCrltsDc() {
+		return crltsDc;
 	}
-	public void setDescription(String description) {
-		this.description = description;
+	public void setCrltsDc(String crltsDc) {
+		this.crltsDc = crltsDc;
 	}
-	public String getName() {
-		return name;
+	public String getCrltsNm() {
+		return crltsNm;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setCrltsNm(String crltsNm) {
+		this.crltsNm = crltsNm;
 	}
-	public String getNameChcrt() {
-		return nameChcrt;
+	public String getCrltsNmChcrt() {
+		return crltsNmChcrt;
 	}
-	public void setNameChcrt(String nameChcrt) {
-		this.nameChcrt = nameChcrt;
+	public void setCrltsNmChcrt(String crltsNmChcrt) {
+		this.crltsNmChcrt = crltsNmChcrt;
 	}
-	public String getNum() {
-		return num;
+	public String getCrltsNo() {
+		return crltsNo;
 	}
-	public void setNum(String num) {
-		this.num = num;
+	public void setCrltsNo(String crltsNo) {
+		this.crltsNo = crltsNo;
 	}
-	public String getNumName() {
-		return numName;
+	public String getCrltsNoNm() {
+		return crltsNoNm;
 	}
-	public void setNumName(String numName) {
-		this.numName = numName;
+	public void setCrltsNoNm(String crltsNoNm) {
+		this.crltsNoNm = crltsNoNm;
 	}
-	public String getLocaleCode() {
-		return localeCode;
+	public String getCtrdCd() {
+		return ctrdCd;
 	}
-	public void setLocaleCode(String localeCode) {
-		this.localeCode = localeCode;
+	public void setCtrdCd(String ctrdCd) {
+		this.ctrdCd = ctrdCd;
 	}
-	public String getLocaleName() {
-		return localeName;
+	public String getCtrdNm() {
+		return ctrdNm;
 	}
-	public void setLocaleName(String localeName) {
-		this.localeName = localeName;
+	public void setCtrdNm(String ctrdNm) {
+		this.ctrdNm = ctrdNm;
 	}
-	public String getImgUrl() {
-		return imgUrl;
+	public String getImageUrl() {
+		return imageUrl;
 	}
-	public void setImgUrl(String imgUrl) {
-		this.imgUrl = imgUrl;
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
 	}
-	public String getImgExist() {
-		return imgExist;
+	public String getImageYn() {
+		return imageYn;
 	}
-	public void setImgExist(String imgExist) {
-		this.imgExist = imgExist;
+	public void setImageYn(String imageYn) {
+		this.imageYn = imageYn;
 	}
-	public String getItemCode() {
-		return itemCode;
+	public String getListImageUrl() {
+		return listImageUrl;
 	}
-	public void setItemCode(String itemCode) {
-		this.itemCode = itemCode;
+	public void setListImageUrl(String listImageUrl) {
+		this.listImageUrl = listImageUrl;
 	}
-	public String getImgPath() {
-		return imgPath;
+	public String getItemCd() {
+		return itemCd;
 	}
-	public void setImgPath(String imgPath) {
-		this.imgPath = imgPath;
+	public void setItemCd(String itemCd) {
+		this.itemCd = itemCd;
 	}
-	public String getCityCode() {
-		return cityCode;
+	public String getItemNm() {
+		return itemNm;
 	}
-	public void setCityCode(String cityCode) {
-		this.cityCode = cityCode;
+	public void setItemNm(String itemNm) {
+		this.itemNm = itemNm;
 	}
-	public String getCity() {
-		return city;
+	public String getSignguCd() {
+		return signguCd;
 	}
-	public void setCity(String city) {
-		this.city = city;
+	public void setSignguCd(String signguCd) {
+		this.signguCd = signguCd;
 	}
-	public String getX() {
-		return x;
+	public String getSignguNm() {
+		return signguNm;
 	}
-	public void setX(String x) {
-		this.x = x;
+	public void setSignguNm(String signguNm) {
+		this.signguNm = signguNm;
 	}
-	public String getY() {
-		return y;
+	public String getXCnts() {
+		return XCnts;
 	}
-	public void setY(String y) {
-		this.y = y;
+	public void setXCnts(String xCnts) {
+		XCnts = xCnts;
+	}
+	public String getYCnts() {
+		return YCnts;
+	}
+	public void setYCnts(String yCnts) {
+		YCnts = yCnts;
 	}
 	
 	
+//	String description; //crltsDC
+//	String name; //crltsNm
+//	String nameChcrt; //crltsNmChcrt
+//	String num; //crltsNo
+//	String numName; // crltsNoNm
+//	String localeCode; // ctrdCd
+//	String localeName; // ctrdNm
+//	String imgUrl; // imageUrl
+//	String imgExist; // imageYn - 1 is exist
+//	String itemCode; //itemCd
+//	String imgPath; // listImageUrl
+//	String cityCode; //signguCd
+//	String city; //signguNm
+//	String x; //xCnts
+//	String y; //yCnts
+//	
+//	public String getDescription() {
+//		return description;
+//	}
+//	public void setDescription(String description) {
+//		this.description = description;
+//	}
+//	public String getName() {
+//		return name;
+//	}
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//	public String getNameChcrt() {
+//		return nameChcrt;
+//	}
+//	public void setNameChcrt(String nameChcrt) {
+//		this.nameChcrt = nameChcrt;
+//	}
+//	public String getNum() {
+//		return num;
+//	}
+//	public void setNum(String num) {
+//		this.num = num;
+//	}
+//	public String getNumName() {
+//		return numName;
+//	}
+//	public void setNumName(String numName) {
+//		this.numName = numName;
+//	}
+//	public String getLocaleCode() {
+//		return localeCode;
+//	}
+//	public void setLocaleCode(String localeCode) {
+//		this.localeCode = localeCode;
+//	}
+//	public String getLocaleName() {
+//		return localeName;
+//	}
+//	public void setLocaleName(String localeName) {
+//		this.localeName = localeName;
+//	}
+//	public String getImgUrl() {
+//		return imgUrl;
+//	}
+//	public void setImgUrl(String imgUrl) {
+//		this.imgUrl = imgUrl;
+//	}
+//	public String getImgExist() {
+//		return imgExist;
+//	}
+//	public void setImgExist(String imgExist) {
+//		this.imgExist = imgExist;
+//	}
+//	public String getItemCode() {
+//		return itemCode;
+//	}
+//	public void setItemCode(String itemCode) {
+//		this.itemCode = itemCode;
+//	}
+//	public String getImgPath() {
+//		return imgPath;
+//	}
+//	public void setImgPath(String imgPath) {
+//		this.imgPath = imgPath;
+//	}
+//	public String getCityCode() {
+//		return cityCode;
+//	}
+//	public void setCityCode(String cityCode) {
+//		this.cityCode = cityCode;
+//	}
+//	public String getCity() {
+//		return city;
+//	}
+//	public void setCity(String city) {
+//		this.city = city;
+//	}
+//	public String getX() {
+//		return x;
+//	}
+//	public void setX(String x) {
+//		this.x = x;
+//	}
+//	public String getY() {
+//		return y;
+//	}
+//	public void setY(String y) {
+//		this.y = y;
+//	}
 }
--- Herimarque-soap-client/src/test/java/net/julnamoo/herimarque/client/soap/request/TestResponseParser.java
+++ Herimarque-soap-client/src/test/java/net/julnamoo/herimarque/client/soap/request/TestResponseParser.java
@@ -1,19 +1,32 @@
 package net.julnamoo.herimarque.client.soap.request;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringReader;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import net.julnamoo.herimarque.client.soap.Item;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 public class TestResponseParser {
 
 	String fpath;
-	BufferedReader br;
 	
 	@Before
 	public void setup()
@@ -21,20 +34,98 @@ public class TestResponseParser {
 	}
 	
 	@Test
-	public void testResponseParsing() throws IOException
+	public void testResponseParsing() throws IOException, ParserConfigurationException, SAXException
 	{
 		File responseDir = new File("response");
 		if(responseDir.isDirectory())
 		{
 			File[] response = responseDir.listFiles();
 			
+			List<Item> resultList = new ArrayList<Item>();
+			
 			for(File f : response)
 			{
 				System.out.println(f.getPath());
-				br = new BufferedReader(new FileReader(f));
-				System.out.println(br.readLine());
+				
+				String msg = FileUtils.readFileToString(f);
+				
+				DocumentBuilder docbuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+				InputSource is = new InputSource(new FileReader(f));
+				is.setCharacterStream(new StringReader(msg));
+				Document doc = docbuilder.parse(is);
+				
+				// get items
+				NodeList items = doc.getElementsByTagName("item");
+				for(int i = 0; i < items.getLength(); ++i)
+				{
+					Node item = items.item(i);
+					NodeList subs = item.getChildNodes();
+					
+					//set field value
+					Item instance = new Item();
+					for(int j = 0; j < subs.getLength(); ++j)
+					{
+						Node sub = subs.item(j);
+						
+						String field = sub.getNodeName();
+						String value = sub.getTextContent();
+						
+						try 
+						{
+							instance = setValue(instance, field, value);
+						} catch (Exception e) 
+						{
+							e.printStackTrace();
+						}
+					}
+					resultList.add(instance);
+				}
+			}
+			
+			if(resultList.size() > 0)
+			{
+				System.out.println("Total size : " + resultList.size());
+				for(Item i : resultList)
+				{
+					System.out.println(i.getItemCd() + ", " + i.getCrltsNm());
+				}
 			}
 		}
+	}
+	
+	
+	private Item setValue(Item item, String field, String value) throws Exception
+	{
+		//convert each field to object field with inflection api
+		
+		Class target = null;
+		
+		try 
+		{
+			target = Class.forName(item.getClass().getName());
+			
+		} catch (ClassNotFoundException e) 
+		{
+			//internal error
+			e.printStackTrace();
+			throw new Exception("Cannot convert object to Item instance");
+		}
+		
+		Field[] fields = target.getDeclaredFields();
+		
+		for(Field f : fields)
+		{
+			if(f.getName().equals(field))
+			{
+				f.setAccessible(true);
+				
+				f.set(item, value);
+				System.out.println(item + ", " + value);
+				return item;
+			}
+		}
+		
+		return null;
 	}
 	
 	@After
