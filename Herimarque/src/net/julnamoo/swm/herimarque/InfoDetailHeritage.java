f594f889a59d968640bd7eb9dc713f56b24b6e48
Julie Kim <kjule114@gmail.com>
2012-02-02 17:08:52 +0900

    complete to build detail view

--- Herimarque/src/net/julnamoo/swm/herimarque/InfoDetailHeritage.java
+++ Herimarque/src/net/julnamoo/swm/herimarque/InfoDetailHeritage.java
@@ -19,7 +19,6 @@ public class InfoDetailHeritage extends Activity {
 		setDetail(item);
 		
 		//for test 
-//		String name = getIntent().getStringExtra(Constants.itemFields[1]);
 		String name = item.getCrltsNm();
 		Log.d(tag, "passed item : " + name);
 		
@@ -27,8 +26,33 @@ public class InfoDetailHeritage extends Activity {
 	
 	private void setDetail(Item item)
 	{
-		//set name
-		TextView tv = (TextView) findViewById(R.id.info_detail_name);
-		tv.setText(item.getCrltsNm());
+		TextView tv;
+		String value;
+		
+		//set name bar
+		value = item.getCrltsNm();
+		tv = (TextView) findViewById(R.id.info_detail_name_bar);
+		tv.setText(value);
+		
+		//set code
+		tv = (TextView) findViewById(R.id.info_detail_code);
+		value = new StringBuilder(item.getItemNm()).append(" ").
+				append(item.getCrltsNoNm()).append("호").toString();
+		tv.setText(value);
+		
+		//set age
+		tv = (TextView) findViewById(R.id.info_detail_age);
+		value = item.getAgeCd();
+		tv.setText(value);
+		
+		//set area
+		tv = (TextView) findViewById(R.id.info_detail_area);
+		value = new StringBuilder(item.getCtrdNm()).append(" ").append(item.getSignguNm()).toString();
+		tv.setText(value);
+		
+		//set description
+		tv = (TextView) findViewById(R.id.info_detail_desc);
+		value = item.getCrltsDc();
+		tv.setText(value);
 	}
 }
