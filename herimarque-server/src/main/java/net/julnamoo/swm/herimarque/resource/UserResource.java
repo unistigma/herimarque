e8e10107211a18b5ba73d95d877a529bfb6eca04
julie_air <kjulee114@gmail.com>
2012-02-07 14:37:37 +0900

    add User Resource Interface

---
+++ herimarque-server/src/main/java/net/julnamoo/swm/herimarque/resource/UserResource.java
@@ -0,0 +1,11 @@
+package net.julnamoo.swm.herimarque.resource;
+
+import javax.ws.rs.core.Response;
+
+public interface UserResource {
+
+	public Response addUser(String key);
+	public Response oauthUser(String key);
+//	public Response changeInfo(Map params);
+	public Response changeKey(String key); // for change e-mail or password
+}
