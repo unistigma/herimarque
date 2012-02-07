3f6c940930885117157c0541caf2dde3b497a683
julie_air <kjulee114@gmail.com>
2012-02-07 17:47:19 +0900

    complete build interface of content resource

--- herimarque-server/src/main/java/net/julnamoo/swm/herimarque/resource/ContentResource.java
+++ herimarque-server/src/main/java/net/julnamoo/swm/herimarque/resource/ContentResource.java
@@ -1,5 +1,22 @@
 package net.julnamoo.swm.herimarque.resource;
 
+import javax.ws.rs.core.Response;
+
 public interface ContentResource {
 
+	/**
+	 * with POST.
+	 * The map photos will be downgraded
+	 * @param key
+	 * @return
+	 */
+	// For Up/Download maps
+	public Response uploadMap(String key); // upload
+	public Response getMyMapList(String key); // retrieve my maps with comments of it
+	public Response getTheOtehrMapList(String email); // retrieve someone's maps with comments of it
+	public Response getLocationMapList(String ctrdCd); //retrieve some place's(using gps location) maps with comments of it
+	public Response getKindMapList(String itemCd); //retrieve maps of the kind with comments of it
+	
+	// For manage comments
+	public Response addComment(String mapKey, String comment);
 }
