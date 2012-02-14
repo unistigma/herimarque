9d16839dbc74fea1bd82fdd8d0ef57fb06dfba0c
Julie <kjulee114@gmail.com>
2012-02-14 21:50:09 +0900

    test mongo annotation

--- herimarque-server/src/main/java/net/julnamoo/swm/herimarque/mongo/SpringMongoConfig.java
+++ herimarque-server/src/main/java/net/julnamoo/swm/herimarque/mongo/SpringMongoConfig.java
@@ -1,14 +1,9 @@
 package net.julnamoo.swm.herimarque.mongo;
 
-import java.io.File;
-import java.io.FileReader;
-import java.util.Properties;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.web.bind.annotation.InitBinder;
 
 import com.mongodb.Mongo;
 
@@ -24,38 +19,38 @@ public class SpringMongoConfig extends AbstractMongoConfiguration{
 	int port;
 	String dbname;
 	
-	@InitBinder
-	public void initBinder()
-	{
-		//set default value
-		host = "localhost";
-		port = 27017;
-		
-		//get values from properties file
-		Properties props = new Properties();
-		File mongoPropF = new File(ClassLoader.getSystemResource("mongo.properties").getFile());
-		try 
-		{
-			props.load(new FileReader(mongoPropF));
-			host = props.getProperty("host", "localhost");
-			port = Integer.parseInt(props.getProperty("port", "27017"));
-		} catch (Exception e) {}
-	}
-	
+//	@InitBinder
+//	public void initBinder()
+//	{
+//		//set default value
+//		host = "localhost";
+//		port = 27017;
+//		
+//		//get values from properties file
+//		Properties props = new Properties();
+//		File mongoPropF = new File(ClassLoader.getSystemResource("mongo.properties").getFile());
+//		try 
+//		{
+//			props.load(new FileReader(mongoPropF));
+//			host = props.getProperty("host", "localhost");
+//			port = Integer.parseInt(props.getProperty("port", "27017"));
+//		} catch (Exception e) {}
+//	}
+//	
 	
 	@Override
 	public String getDatabaseName() {
-		return this.dbname;
+		return "herimarque";
 	}
 
 	@Override
 	public @Bean Mongo mongo() throws Exception {
 		
-		return new Mongo();
+		return new Mongo("localhost");
 	}
 
 	@Override
 	public @Bean MongoTemplate mongoTemplate() throws Exception {
-		return new MongoTemplate(mongo(), this.dbname);
+		return new MongoTemplate(mongo(), "herimarque");
 	}
 }
