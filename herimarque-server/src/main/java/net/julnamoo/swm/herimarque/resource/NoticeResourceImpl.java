e77e56cc676da280161f10b4d15f62e160e618a6
julnamoo <kjulee114@gmail.com>
2012-02-21 17:06:44 +0900

    change response of the notice resource for building different response

--- herimarque-server/src/main/java/net/julnamoo/swm/herimarque/resource/NoticeResourceImpl.java
+++ herimarque-server/src/main/java/net/julnamoo/swm/herimarque/resource/NoticeResourceImpl.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import net.julnamoo.swm.herimarque.model.Notice;
 import net.julnamoo.swm.herimarque.util.ConstantsBean;
@@ -23,7 +24,16 @@ public class NoticeResourceImpl implements NoticeResource {
 	public Response isExist(String lastupdate) 
 	{
 		boolean isExist = true;
-		Response response = Response.ok().header("exist", isExist).build();
+		
+		Response response = null;
+		
+		if(isExist)
+		{
+			response = Response.ok().build();
+		}else
+		{
+			response = Response.status(Status.NO_CONTENT).build();
+		}
 		return response;
 	}
 
