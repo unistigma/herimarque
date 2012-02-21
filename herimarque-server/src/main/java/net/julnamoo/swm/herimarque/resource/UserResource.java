41a65d70970f994a60dd5ee48b690eab0fc06044
julnamoo <kjulee114@gmail.com>
2012-02-21 17:19:58 +0900

    change user resource interface for delete user and add user params

--- herimarque-server/src/main/java/net/julnamoo/swm/herimarque/resource/UserResource.java
+++ herimarque-server/src/main/java/net/julnamoo/swm/herimarque/resource/UserResource.java
@@ -4,7 +4,8 @@ import javax.ws.rs.core.Response;
 
 public interface UserResource {
 
-	public Response addUser(String key);
+	public Response addUser(String email, String pwd);
+	public Response delUser(String key, String email);
 	public Response oauthUser(String key);
 	public Response changeKey(String key); // for change e-mail or password
 }
