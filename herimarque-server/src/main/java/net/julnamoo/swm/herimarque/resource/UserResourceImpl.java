d599414cb5e83801d971571d57d456912470bd2b
Julie Kim <kjule114@gmail.com>
2012-02-23 04:15:46 +0900

    build User resource interface

--- herimarque-server/src/main/java/net/julnamoo/swm/herimarque/resource/UserResourceImpl.java
+++ herimarque-server/src/main/java/net/julnamoo/swm/herimarque/resource/UserResourceImpl.java
@@ -3,6 +3,7 @@ package net.julnamoo.swm.herimarque.resource;
 import javax.annotation.Resource;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -11,54 +12,71 @@ import javax.ws.rs.core.Response.Status;
 
 import net.julnamoo.swm.herimarque.util.ConstantsBean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Path("/u")
 @Component
 public class UserResourceImpl implements UserResource {
 
+	Logger logger = LoggerFactory.getLogger(UserResourceImpl.class.getSimpleName());
+	
 	@Resource
 	private ConstantsBean constants;
 	
 	@POST
+	
 	@Override
-	public Response addUser(String email, String pwd) 
+	public Response addUser(@HeaderParam("email") String email, @HeaderParam("pwd") String pwd) 
 	{
+		logger.debug("start handling addUser, {}", email);
+		
 		String key = "tempKey";
-		Response response = Response.status(Status.CREATED).entity(key).build();
+		Response response = Response.status(Status.CREATED).header("key",key).build();
+		
+		logger.debug("return 200");
 		return response;
 	}
 
 	@DELETE
 	@Override
-	public Response delUser(String key, String email) 
+	public Response delUser(@HeaderParam("key") String key, @HeaderParam("email") String email) 
 	{
+		logger.debug("start hanndling delUser");
+		
 		boolean isDeleted = true;
 		Response response = null;
 		
 		if(isDeleted)
 		{
 			response = Response.ok().build();
+			logger.debug("return 200");
 		}else
 		{
 			response = Response.status(Status.SERVICE_UNAVAILABLE).build();
+			logger.debug("return 503");
 		}
 		return response;
 	}
 	
 	@GET
 	@Override
-	public Response oauthUser(String key) 
+	public Response oauthUser(@HeaderParam("key") String key) 
 	{
+		logger.debug("start handling oauthUser");
+		
 		Response response = null;
 		boolean Authentication = true;
 		
 		if(Authentication)
 		{
 			response = Response.status(Status.ACCEPTED).build();
+			logger.debug("return 202");
 		}else
 		{
 			response = Response.status(Status.UNAUTHORIZED).build();
+			logger.debug("return 401");
 		}
 		
 		return response;
@@ -66,17 +84,21 @@ public class UserResourceImpl implements UserResource {
 
 	@PUT
 	@Override
-	public Response changeKey(String key, String pwd) 
+	public Response changeKey(@HeaderParam("key") String key, @HeaderParam("pwd") String pwd) 
 	{
+		logger.debug("start handling changeKey");
+		
 		Response response = null;
 		boolean changed = true;
 		
 		if(changed)
 		{
-			response = Response.status(Status.OK).entity(new String("newkey")).build();
+			response = Response.status(Status.OK).header("key", new String("newkey")).build();
+			logger.debug("return 200");
 		}else
 		{
 			response = Response.status(Status.NOT_ACCEPTABLE).build();
+			logger.debug("return 406");
 		}
 		return response;
 	}
