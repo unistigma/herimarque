00c852e6b38babb6862b34a6dce806aba20540a6
Julie Kim <kjule114@gmail.com>
2012-02-24 04:23:50 +0900

    add User POJO for mongo

---
+++ herimarque-server/src/main/java/net/julnamoo/swm/herimarque/model/User.java
@@ -0,0 +1,52 @@
+package net.julnamoo.swm.herimarque.model;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.hamcrest.core.IsInstanceOf;
+
+@XmlRootElement
+public class User implements Serializable{
+
+	String email;
+	String finalKey;
+	boolean auth;
+
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFinalKey() {
+		return finalKey;
+	}
+	public void setFinalKey(String finalKey) {
+		this.finalKey = finalKey;
+	}
+	public boolean isAuth() {
+		return auth;
+	}
+	public void setAuth(boolean auth) {
+		this.auth = auth;
+	}
+
+	@Override
+	public boolean equals(Object obj) 
+	{
+		if(User.class.isInstance(obj))
+		{
+			User user = (User) obj;
+
+			return user.getEmail().equals(this.getEmail());
+		}else
+			return false;
+	}
+	
+	@Override
+	public String toString() 
+	{
+		return this.getEmail();
+	}
+}
