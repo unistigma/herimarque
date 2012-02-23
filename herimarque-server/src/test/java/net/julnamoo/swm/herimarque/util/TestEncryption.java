14de139dfb9de77ac2bf8ab438ffa75717272943
Julie Kim <kjule114@gmail.com>
2012-02-23 20:34:07 +0900

    add encryption test case with MD5

---
+++ herimarque-server/src/test/java/net/julnamoo/swm/herimarque/util/TestEncryption.java
@@ -0,0 +1,33 @@
+package net.julnamoo.swm.herimarque.util;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestEncryption 
+{
+	String email;
+	String pwd;
+	
+	@Before
+	public void setup()
+	{
+		email = "test@test.com";
+		pwd = "test";
+	}
+	
+	@Test
+	public void test() throws Exception
+	{
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		md.reset();
+		md.update(email.getBytes("UTF-8"));
+		md.update(pwd.getBytes("UTF-8"));
+		BigInteger bi = new BigInteger(md.digest());
+		String key = bi.toString();
+		System.out.println("new key : " + key + ", length is " + key.length());
+	}
+
+}
