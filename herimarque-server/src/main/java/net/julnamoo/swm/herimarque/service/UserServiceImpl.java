499d017c793c4f4868d9ffb8ffe00ccc645de318
Julie Kim <kjule114@gmail.com>
2012-02-24 16:17:33 +0900

    add useradd service

--- herimarque-server/src/main/java/net/julnamoo/swm/herimarque/service/UserServiceImpl.java
+++ herimarque-server/src/main/java/net/julnamoo/swm/herimarque/service/UserServiceImpl.java
@@ -1,8 +1,12 @@
 package net.julnamoo.swm.herimarque.service;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import net.julnamoo.swm.herimarque.dao.UserDAOImpl;
+import net.julnamoo.swm.herimarque.util.MailSender;
+import net.julnamoo.swm.herimarque.util.UserInfoEncryptor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,15 +17,36 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService {
 	
+	Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
+	@Autowired
+	UserDAOImpl userDAO;
+	
 	@Override
-	public String addUser(String email, String pwd) {
-		// TODO Auto-generated method stub
-		return null;
+	public String addUser(String email, String pwd) 
+	{
+		// generate the temporal key and insert to Mongo
+		logger.debug("Generate temp key for respected user {}", email);
+		String key = UserInfoEncryptor.encryption(email, pwd);
+		String _id = userDAO.addUser(email, key);
+		
+		/** Send the email **/
+		new MailSender().sendMail(email, key);
+		return _id;
 	}
 
+	/**
+	 * Unauthorized user are removed by admin.
+	 * Generate the real key with email and the key
+	 */
 	@Override
-	public boolean oauthUser(String email, String key) {
-		// TODO Auto-generated method stub
+	public boolean oauthUser(String email, String key) 
+	{
+		//generate the final key
+		logger.debug("Generate final key for new user {}", email);
+		String finalKey = UserInfoEncryptor.encryption(email, key);
+		
+		
 		return false;
 	}
 
