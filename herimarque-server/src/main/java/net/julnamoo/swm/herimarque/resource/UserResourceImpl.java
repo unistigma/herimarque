165970b64f44791f9d6e39d22d12e06f9d8baf7f
Julie Kim <kjule114@gmail.com>
2012-02-24 16:18:31 +0900

    change the logger constructor from simple name of class to reflection of the class

--- herimarque-server/src/main/java/net/julnamoo/swm/herimarque/resource/UserResourceImpl.java
+++ herimarque-server/src/main/java/net/julnamoo/swm/herimarque/resource/UserResourceImpl.java
@@ -7,7 +7,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -20,7 +19,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserResourceImpl implements UserResource {
 
-	Logger logger = LoggerFactory.getLogger(UserResourceImpl.class.getSimpleName());
+	Logger logger = LoggerFactory.getLogger(UserResourceImpl.class);
 	
 	/**
 	 * Enrolling the user
