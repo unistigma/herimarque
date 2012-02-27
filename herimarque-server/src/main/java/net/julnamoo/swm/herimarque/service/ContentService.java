d7edfd7c8a4ce3bcc5ab31b98e9910615a44a804
Julie Kim <kjule114@gmail.com>
2012-02-28 02:32:00 +0900

    add insert map info to mongo at uploadMap in ContentService

--- herimarque-server/src/main/java/net/julnamoo/swm/herimarque/service/ContentService.java
+++ herimarque-server/src/main/java/net/julnamoo/swm/herimarque/service/ContentService.java
@@ -4,6 +4,13 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Date;
+
+import javax.annotation.Resource;
+
+import net.julnamoo.swm.herimarque.dao.ContentsDAOImpl;
+import net.julnamoo.swm.herimarque.model.MapInfo;
+import net.julnamoo.swm.herimarque.util.PropertiesUtil;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,14 +21,31 @@ public class ContentService {
 
 	Logger logger = LoggerFactory.getLogger(ContentService.class);
 	
-	public void uploadMap(InputStream uploadedInputStream, String uploadedFileLocation)
+	@Resource(name="contentsDAO")
+	ContentsDAOImpl contentsDAO;
+	
+	public void uploadMap(InputStream uploadedInputStream, MapInfo mapInfo)
 	{
-		logger.debug("Start to write map {}", uploadedFileLocation);
+		logger.debug("Start to write {}", mapInfo.getFilePath());
 		
+		StringBuilder sb = new StringBuilder();
+		String repo = PropertiesUtil.getValueFromProperties("herimarque.properties", "mapRepo");
+		sb.append(repo).append(File.separatorChar).append(mapInfo.getUser());
+		String dirPath = sb.toString();
+		
+		File userDir = new File(dirPath);
+		
+		if(!userDir.exists())
+		{
+			userDir.mkdirs();
+		}
+		
+		//CAN BE PROCESSED with THREAD for taking SHORTER TIME for saving the image
+		String mapPath = mapInfo.getFilePath();
 		//It needs downgrade the resolution of the map picture
 		try
 		{
-			OutputStream os = new FileOutputStream(new File(uploadedFileLocation));
+			OutputStream os = new FileOutputStream(new File(mapPath));
 			
 			int read = 0;
 			int size = 0;
@@ -37,11 +61,16 @@ public class ContentService {
 			os.flush();
 			os.close();
 			
-			logger.debug("{} >> Total size : {}", uploadedFileLocation, size);
-			logger.info("Complete to save {}", uploadedFileLocation);
+			logger.debug("{} >> Total size : {}", mapPath, size);
+			logger.info("Complete to save {}", mapPath);
 		}catch (Exception e) 
 		{
 			e.printStackTrace();
 		}
+		
+		Date now = new Date();
+		mapInfo.setUploadTime(now);
+		//save other information of the map to the mongo
+		contentsDAO.addMapInfo(mapInfo);
 	}
 }
