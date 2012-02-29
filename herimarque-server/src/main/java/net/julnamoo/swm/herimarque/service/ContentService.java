9b41ff23cdf9275f744a6f82065879e22f0a73e2
Julie Kim <kjule114@gmail.com>
2012-02-29 23:59:55 +0900

    build upload map

--- herimarque-server/src/main/java/net/julnamoo/swm/herimarque/service/ContentService.java
+++ herimarque-server/src/main/java/net/julnamoo/swm/herimarque/service/ContentService.java
@@ -4,14 +4,18 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.annotation.Resource;
 
 import net.julnamoo.swm.herimarque.dao.ContentsDAOImpl;
+import net.julnamoo.swm.herimarque.dao.UserDAOImpl;
 import net.julnamoo.swm.herimarque.model.MapInfo;
 import net.julnamoo.swm.herimarque.util.PropertiesUtil;
 
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -24,12 +28,18 @@ public class ContentService {
 	@Resource(name="contentsDAO")
 	ContentsDAOImpl contentsDAO;
 	
+	@Resource(name="userDAO")
+	UserDAOImpl userDAO;
+	
 	public void uploadMap(InputStream uploadedInputStream, MapInfo mapInfo)
 	{
 		logger.debug("Start to write {}", mapInfo.getFilePath());
 		
+		String user = mapInfo.getUser();
+		userDAO.getUserKey(user);
+		
 		StringBuilder sb = new StringBuilder();
-		String repo = PropertiesUtil.getValueFromProperties("herimarque.properties", "mapRepo");
+		String repo = PropertiesUtil.getValueFromProperties("herimarque.properties", "mapsRepo");
 		sb.append(repo).append(File.separatorChar).append(mapInfo.getUser());
 		String dirPath = sb.toString();
 		
@@ -72,5 +82,13 @@ public class ContentService {
 		mapInfo.setUploadTime(now);
 		//save other information of the map to the mongo
 		contentsDAO.addMapInfo(mapInfo);
+	}
+	
+	public List<String> getMyMapList(String id)
+	{
+		List<String> myMapList = new ArrayList<String>();
+		
+		
+		return myMapList;
 	}
 }
