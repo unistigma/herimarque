da29639e34457bab06ba4a90f60fdd6f85d1c670
Julie Kim <kjule114@gmail.com>
2012-03-01 21:06:12 +0900

    change the addUser and getUserKey function for return Oid

--- herimarque-server/src/main/java/net/julnamoo/swm/herimarque/dao/UserDAOImpl.java
+++ herimarque-server/src/main/java/net/julnamoo/swm/herimarque/dao/UserDAOImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
@@ -49,11 +50,14 @@ public class UserDAOImpl extends SimpleHerimarqueDAO implements UserDAO
 			DBObject qdoc = new BasicDBObject();
 			qdoc.put("id", id);
 			collection.update(qdoc, doc);
-			_id = null;
+			BasicDBObject fdoc = (BasicDBObject) collection.findOne(qdoc);
+			ObjectId oid = (ObjectId) fdoc.get("_id");
+			_id = String.valueOf(oid.getInc());
 		}else
 		{
 			collection.save(doc);
-			_id = doc.get("_id").toString();
+			ObjectId oid = (ObjectId) doc.get("_id");
+			_id = oid.toString();
 		}
 		logger.debug("exec save with a object, {}", doc);
 		return _id;//doc.get("_id").toString();
@@ -69,13 +73,14 @@ public class UserDAOImpl extends SimpleHerimarqueDAO implements UserDAO
 
 		logger.info("Retrive key of {}", id);
 		DBCursor cursor = collection.find(doc);
-		if(cursor.size() > 0)
+		if(cursor.size() == 0)
 		{
 			return null;
 		}else
 		{
 			DBObject resultDoc = cursor.next();
-			return cursor.next().get("finalKey").toString();
+			ObjectId oid = (ObjectId) resultDoc.get("_id");
+			return oid.toString();
 		}
 	}
 
@@ -92,8 +97,10 @@ public class UserDAOImpl extends SimpleHerimarqueDAO implements UserDAO
 		doc.put("finalKey", newKey);
 		collection.save(doc);
 
+		ObjectId oid = (ObjectId) doc.get("_id");
+		String _id = oid.toString();
 		logger.debug("change Info of {} with {}", id, newKey);
-		return doc.get("_id").toString();
+		return _id;
 	}
 
 	@Override
@@ -105,14 +112,8 @@ public class UserDAOImpl extends SimpleHerimarqueDAO implements UserDAO
 		DBObject qdoc = new BasicDBObject();
 		qdoc.put("id", id);
 		
-		DBCursor results = collection.find(qdoc);
-		logger.info("Find {} users with {}, all of them are removed", results.size(), id);
-		while(results.hasNext())
-		{
-			DBObject target = results.next();
-			collection.remove(target);
-			logger.debug("remove {}", target);
-		}
+		collection.remove(qdoc);
+		logger.debug("remove the user {} info from the mongo", id);
 	}
 
 	public List<String> allUsers()
