08995cb6ff3ebb950c20d5cea83d32bf1ba086dc
Julie Kim <kjule114@gmail.com>
2012-03-03 20:43:04 +0900

    add side files

--- herimarque-server/src/main/java/net/julnamoo/swm/herimarque/util/MailSender.java
+++ herimarque-server/src/main/java/net/julnamoo/swm/herimarque/util/MailSender.java
@@ -6,14 +6,12 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Properties;
 
-import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 
 import org.slf4j.Logger;
