9e21d1b5986ca9e1936fc20bca56a4ae0f06cf9d
Julie Kim <kjule114@gmail.com>
2012-03-08 21:19:03 +0900

    modify dependency of user resource

--- herimarque-server/src/main/java/net/julnamoo/swm/herimarque/resource/UserResourceImpl.java
+++ herimarque-server/src/main/java/net/julnamoo/swm/herimarque/resource/UserResourceImpl.java
@@ -18,8 +18,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.google.gson.Gson;
-
 @Path("/u")
 @Component
 public class UserResourceImpl implements UserResource {
