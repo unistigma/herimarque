cdba718360e107e0885ae8d30d8bcfcd71b837a3
Julie Kim <kjule114@gmail.com>
2012-03-19 19:04:31 +0900

    change the loading dialog fragment using progress dialog

--- Herimarque/src/net/julnamoo/swm/herimarque/info/LoadingFragment.java
+++ Herimarque/src/net/julnamoo/swm/herimarque/info/LoadingFragment.java
@@ -1,21 +1,30 @@
 package net.julnamoo.swm.herimarque.info;
 
-import net.julnamoo.R;
-import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 
 public class LoadingFragment extends DialogFragment {
-	public LoadingFragment() {	}
+	public LoadingFragment() {	
+	}
 
+	@Override
+	public void onCreate(Bundle arg0) 
+	{
+		int style = DialogFragment.STYLE_NO_FRAME;
+		int theme = android.R.style.Theme_Light;
+		setStyle(style, theme);
+		super.onCreate(arg0);
+	}
+	
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-		return new AlertDialog.Builder(getActivity())
-		.setIcon(R.drawable.progress)
-		.setTitle("Loading...")
-		.create();
+		ProgressDialog dialog = new ProgressDialog(getActivity());
+		dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		dialog.setMessage("Loading...");
+		return dialog;
 	}
 
 }
